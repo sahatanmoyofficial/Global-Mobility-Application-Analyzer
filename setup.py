38965418b56d0c47a8d2d9be293cff9d895a9d23
Tanmoy Saha <sahatanmoyofficial@gmail.com>
2026-03-11 10:35:06 +0100

    loggers and utils added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="visa",
     version="0.0.0",
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Tanmoy Saha",
+    author_email="sahatanmoyofficial@gmail.com",
     packages=find_packages()
 )
